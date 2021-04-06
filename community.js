c43e54a9dab1ee9b41a83ab7af67f4924b1d9ed2
Achintya Mengi <am557@st-andrews.ac.uk>
2021-04-06 13:46:08 +0100

    Added community

--- community.js
+++ community.js
@@ -10,14 +10,20 @@ exports.makeUser = (userName, userEmail) => {
         this.interests = [];
         this.gender = null;
         this.age = null;
+        this.adminCommunities = [];
 
         /**
          * Join a new community.
          * @param {Object} community
          * */
-        this.joinCommunity = function(community) {
+        this.joinCommunity = function(community,isAdmin = false) {
             if (!(this.communities.includes(community))) {
                 this.communities.push(community)
+            }
+
+            if(isAdmin === true && !(this.communities.includes(community)))
+            {
+                this.adminCommunities.push(community)
             }
         }
 
@@ -26,7 +32,10 @@ exports.makeUser = (userName, userEmail) => {
          * @param {Object} community
          * */
         this.leaveCommunity = function(community) {
+            if(!this.adminCommunities.includes(community))
+            {
             this.communities.splice(this.communities.indexOf(community), 1)
+            }
         }
 
         /**
@@ -51,3 +60,33 @@ exports.makeUser = (userName, userEmail) => {
     return new User()
 }
 
+
+
+exports.makeCommunity = (CommunityName, Admin) => {
+    function Community () {
+        this.id = uuid()
+        this.CommunityName = CommunityName;
+        this.Admin = Admin;
+        this.users = []
+
+        this.addUser = function(user, isAdmin = false) {
+            if (!(this.users.includes(user))) {
+                this.users.push(user)
+                user.joinCommunity(this,isAdmin)
+
+            }
+        }
+        this.addUser(this.Admin, isAdmin = true )
+
+        //Leaving the community
+        this.removeUser = function(user) {
+            if (user !== this.Admin)
+            {
+                this.users.splice(this.users.indexOf(user), 1)
+                user.leaveCommunity(this)
+            }
+        }
+        
+    }
+    return new Community()
+}
