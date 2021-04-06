4611fd49b13fd3995f1105c1d8a7a28c54fb7d2e
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-04-06 13:15:09 +0100

    Added User object to community.js

--- community.js
+++ community.js
@@ -1,10 +1,53 @@
+const { uuid } = require('uuid')
+
 exports.makeUser = (userName, userEmail) => {
     function User () {
+        this.id = uuid()
         this.userName = userName;
         this.userEmail = userEmail;
-        this.changeName = function (newName) {
-            this.userName = newName;
-        } 
+        this.location = null;
+        this.communities = [];
+        this.interests = [];
+        this.gender = null;
+        this.age = null;
+
+        /**
+         * Join a new community.
+         * @param {Object} community
+         * */
+        this.joinCommunity = function(community) {
+            if (!(this.communities.includes(community))) {
+                this.communities.push(community)
+            }
+        }
+
+        /**
+         * Leave a new community.
+         * @param {Object} community
+         * */
+        this.leaveCommunity = function(community) {
+            this.communities.splice(this.communities.indexOf(community), 1)
+        }
+
+        /**
+         * Add a new interest.
+         * @param {string} interest
+         * */
+        this.addInterest = function(interest) {
+            if (!(this.interests.includes(interest))) {
+                this.interests.push(interest)
+            }
+        }
+
+        /**
+         * Remove an interest.
+         * @param {string} interest
+         * */
+        this.removeInterest = function(interest) {
+            this.interests.splice(this.interests.indexOf(interest), 1)
+        }
+
     }
     return new User()
 }
+
