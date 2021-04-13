20ef570130bd15c07195abe7fd3670c72305b4cf
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-04-13 09:58:50 +0100

    Added feed to dashboard

--- Content/js/modules/dashboard.js
+++ Content/js/modules/dashboard.js
@@ -19,7 +19,7 @@ const makeShortlistVue = function() {
                 {name: 'Tennis', id: 2},
                 {name: 'Painting', id: 3},
                 {name: 'Cooking', id: 4}
-            ]
+            ],
         },
         methods: {
             goToCommunity: function(communityId) {
@@ -32,11 +32,61 @@ const makeShortlistVue = function() {
 const makeFeedVue = function() {
     const feedVue = new Vue({
         el: '#feed',
+        data: {
+            mostRecentActivities: [
+                {
+                    community: {name: 'gardening', id: '1'},
+                    thread: {name: 'Spaceships', id: 1},
+                    comment: {author: 'realdonaldtrump', time: '06/12/2015, 22:12', text: 'Despite the constant negative press covfefe',}
+                },
+                {
+                    community: {name: 'gardening', id: '1'},
+                    thread: {name: 'Gigafactory', id: 2},
+                    comment: {author: 'whitequeen', time: '30/07/2016, 23:12', text: 'Beware the Jubjub bird, and shun the frumious Bandersnatch',}
+                },
+                {
+                    community: {name: 'gardening', id: '1'},
+                    thread: {name: 'Gigafactory', id: 2},
+                    comment: {author: 'whitequeen', time: '30/07/2016, 23:12', text: 'Beware the Jubjub bird, and shun the frumious Bandersnatch',}
+                },
+                {
+                    community: {name: 'gardening', id: '1'},
+                    thread: {name: 'Gigafactory', id: 2},
+                    comment: {author: 'whitequeen', time: '30/07/2016, 23:12', text: 'Beware the Jubjub bird, and shun the frumious Bandersnatch',}
+                },
+                {
+                    community: {name: 'gardening', id: '1'},
+                    thread: {name: 'Gigafactory', id: 2},
+                    comment: {author: 'whitequeen', time: '30/07/2016, 23:12', text: 'Beware the Jubjub bird, and shun the frumious Bandersnatch',}
+                },
+                {
+                    community: {name: 'gardening', id: '1'},
+                    thread: {name: 'Gigafactory', id: 2},
+                    comment: {author: 'whitequeen', time: '30/07/2016, 23:12', text: 'Beware the Jubjub bird, and shun the frumious Bandersnatch',}
+                },
+                {
+                    community: {name: 'gardening', id: '1'},
+                    thread: {name: 'Gigafactory', id: 2},
+                    comment: {author: 'whitequeen', time: '30/07/2016, 23:12', text: 'Beware the Jubjub bird, and shun the frumious Bandersnatch',}
+                },
+                {
+                    community: {name: 'gardening', id: '1'},
+                    thread: {name: 'Gigafactory', id: 2},
+                    comment: {author: 'whitequeen', time: '30/07/2016, 23:12', text: 'Beware the Jubjub bird, and shun the frumious Bandersnatch',}
+                },
+            ],
+        },
         computed: {
             state() {
                 return client.state;
             },
         },
+        methods: {
+            goToCommunity: function(communityId) {
+                // todo get right community
+                setState('community')
+            },
+        }
     })
 }
 
