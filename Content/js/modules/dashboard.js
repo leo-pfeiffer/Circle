9b4f7190d962a3c65b24bedfcd63564869397a22
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-04-13 21:55:26 +0100

    Added calendar to dashboard

--- Content/js/modules/dashboard.js
+++ Content/js/modules/dashboard.js
@@ -2,7 +2,7 @@
  * This file contains the vue components of the dashboard.
  * */
 
-import {client, goToCommunity, setState} from './clientUtils.js'
+import {client, goToCommunity, isDateMatch, setState, timeOfDayFormatter} from './clientUtils.js'
 
 const makeDashboardHeaderVue = function() {
     const dashboardHeaderVue = new Vue({
@@ -104,12 +104,51 @@ const makeFeedVue = function() {
 
  const makeCalendarPreviewVue = function() {
      const makeCalendarPreview = new Vue({
-         el: '#calendarPreview',
+         el: '#calendar-preview',
+         data: {
+             selectedDate: null,
+             events: [
+                 {title: 'Brunch', desciption: 'Just brunch.. ', community: {name: 'Gardening', id: 1}, organiser: {username: 'lebron', id: 123}, datetime: new Date(2021, 3, 1, 10, 0)},
+                 {title: 'Lunch', desciption: 'Just lunch.. ', community: {name: 'Gardening', id: 1}, organiser: {username: 'lebron', id: 123}, datetime: new Date(2021, 3, 10, 13, 0)},
+                 {title: 'Breakfast', desciption: 'Just breakhast.. ', community: {name: 'Gardening', id: 1}, organiser: {username: 'lebron', id: 123}, datetime: new Date(2021, 3, 10, 9, 0)},
+                 {title: 'Tea', desciption: 'Just tea.. ', community: {name: 'Gardening', id: 1}, organiser: {username: 'lebron', id: 123}, datetime: new Date(2021, 3, 22, 17, 0)},
+                 {title: 'Brunch', desciption: 'Just brunch.. ', community: {name: 'Gardening', id: 1}, organiser: {username: 'lebron', id: 123}, datetime: new Date(2021, 3, 6, 10, 0)},
+                 {title: 'Brunch', desciption: 'Just brunch.. ', community: {name: 'Gardening', id: 1}, organiser: {username: 'lebron', id: 123}, datetime: new Date(2021, 3, 16, 11, 0)},
+             ],
+         },
          computed: {
              state() {
                  return client.state;
              },
+             attributes() {
+                 return [{
+                     bar: {backgroundColor: 'blue'},
+                     dates: this.events.map(el => el.datetime),
+                 }]
+             },
+             /**
+              * Return all events of the currently selected day sorted by time of event.
+              * */
+             eventsOnSelectedDay() {
+                 if (this.selectedDate === null) {
+                     return [];
+                 }
+                 return this.events.filter(el => isDateMatch(el.datetime, this.selectedDate))
+                     .sort((a, b) => a.datetime - b.datetime)
+             }
          },
+         methods: {
+             goToCommunity: function(communityId) {
+                 goToCommunity(communityId)
+             },
+             /**
+              * Return hour and minute in a nicer format
+              * @param {Date} datetime
+              * */
+             timeOfDayFormatter: function(datetime) {
+                 return timeOfDayFormatter(datetime)
+             }
+         }
      })
  }
 
