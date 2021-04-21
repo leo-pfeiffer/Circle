b32c7cea3ced7de017ba9cdcca5bd7bba6253c9c
Ann-Kathrin Losse <akl7@st-andrews.ac.uk>
2021-04-21 10:29:06 +0200

    Pushed Jianis update on the weather and maps API

--- API/app.js
+++ API/app.js
@@ -5,8 +5,9 @@ let MAP_URL = `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_to
 const WEATHER_ACCESS_TOKEN = "XJPAKEZMXS2RTGNE2HXA7BSKP";
 // the URL to get the weather information
 let WEATHER_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/$_coordinate?key=${WEATHER_ACCESS_TOKEN}`;
+let WEATHER_URL_DATE = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/$_coordinate/$_startdate/$_enddate?key=${WEATHER_ACCESS_TOKEN}`;
 // console.log(r);
-const map = L.map('map').setView([51.505,-0.09], 13);
+const map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer(MAP_URL, {
     maxZoom: 23,
     id: 'mapbox/streets-v11',
@@ -42,28 +43,61 @@ const getWindDirection = (degree) => {
     return "";
 }
 
-map.on('click', async function (e) {
-    const coordinates = e.latlng;
-    const coord = `${coordinates.lat},${coordinates.lng}`;
-    const weatherRequestUrl = WEATHER_URL.replace("$_coordinate", coord);
-    // console.log(weatherRequestUrl);
-    const res = await fetch(weatherRequestUrl);
-    const data = await res.json();
-    const weatherInfo = data.currentConditions;
-    // console.log(data);
-    const popup = L.popup();
+const info = (weatherInfo) => {
     let _html = "";
     const windDirection = getWindDirection(weatherInfo.winddir)
-    _html += `<p class='info'>${weatherInfo.conditions}</p>`;
-    _html += `<p class='info'>${data.description}</p>`;
     _html += `<div><img src="img/temp.png" width="15" height="15" /> <span class='info'>${weatherInfo.temp}°C</span></div>`;
     _html += `<div><img src="img/humidity.png" width="15" height="15" /> <span class='info'>${weatherInfo.humidity}%</span></div>`;
     _html += `<div><img src="img/precip.png" width="15" height="15" /> <span class='info'>${weatherInfo.precip}%</span></div>`;
     _html += `<div><img src="img/winddir.png" width="15" height="15" /> <span class='info'>${windDirection} wind</span></div>`;
     _html += `<div><img src="img/windspeed.png" width="15" height="15" /> <span class='info'>${weatherInfo.windspeed}m/s</span></div>`;
     _html += `<div><img src="img/visibility.png" width="15" height="15" /> <span class='info'>${weatherInfo.visibility}m</span></div>`;
-    popup
-        .setLatLng(e.latlng)
-        .setContent(_html)
-        .openOn(map);
+    return _html;
+}
+
+map.on('click', async function (e) {
+    const coordinates = e.latlng;
+    const coord = `${coordinates.lat},${coordinates.lng}`;
+    const startDate = document.getElementById('start-date').value;
+    const endDate = document.getElementById('end-date').value;
+    console.log(startDate, endDate);
+    if (startDate && endDate) {
+        const weatherRequestUrl = WEATHER_URL_DATE.replace("$_coordinate", coord)
+            .replace("$_startdate", startDate)
+            .replace("$_enddate", endDate);
+        const res = await fetch(weatherRequestUrl);
+        const data = await res.json();
+        if (data.days && data.days.length > 0) {
+            const popup = L.popup();
+            let _html = '';
+            _html += `<div class='container'>`;
+            _html += '<p class="info">Future weather forecast</p>';
+            for (const day of data.days) {
+                _html += '<div>';
+                _html += `<div><span class='info'>Date</span>&nbsp;&nbsp;<span class='info'>${day.datetime}</span></div>`;
+                _html += info(day);
+                _html += '</div>';
+                _html += '<hr/>';
+            }
+            _html += '</div>';
+            popup.setLatLng(e.latlng)
+                .setContent(_html)
+                .openOn(map);
+        }
+    } else {
+        const weatherRequestUrl = WEATHER_URL.replace("$_coordinate", coord);
+        // console.log(weatherRequestUrl);
+        const res = await fetch(weatherRequestUrl);
+        const data = await res.json();
+        const weatherInfo = data.currentConditions;
+        // console.log(data);
+        const popup = L.popup();
+        let _html = "";
+        _html += `<p class='info'>${weatherInfo.conditions}</p>`;
+        _html += `<p class='info'>${data.description}</p>`;
+        _html += info(weatherInfo);
+        popup.setLatLng(e.latlng)
+            .setContent(_html)
+            .openOn(map);
+    }
 });
